c32b5657f5ae4c0eb3d787c539eeeb4ef8739533
rtouti <touti.raouf@gmail.com>
2017-04-11 17:53:40 -0400

    Ajout d'un menu principal (incomplet)

---
+++ app/src/main/java/com/reddit/client/redditclient2/controllers/activities/SplashScreen.java
@@ -0,0 +1,20 @@
+package com.reddit.client.redditclient2.controllers.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.reddit.client.redditclient2.R;
+import com.reddit.client.redditclient2.controllers.listeners.MainMenuOnClickListener;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Button news_button = (Button)findViewById(R.id.news_button);
+        news_button.setOnClickListener(new MainMenuOnClickListener(this));
+    }
+}
---
+++ app/src/main/java/com/reddit/client/redditclient2/controllers/listeners/MainMenuOnClickListener.java
@@ -0,0 +1,8 @@
+package com.reddit.client.redditclient2.controllers.listeners;
+
+/**
+ * Created by raouf on 17-04-11.
+ */
+
+public class MainMenuOnClickListener {
+}
