4f5a1ef67980dc38a0cfad98bcb9d6a7983e44d9
Judelin <seide_judelin@yahoo.fr>
2017-04-23 10:25:48 -0400

    vous pouvez regarder la modification dans listeners dans le TextSizeAndShareOnClick...

--- app/src/main/java/com/reddit/client/redditclient2/controllers/listeners/TextSizeAndShareOnClickListener.java
+++ app/src/main/java/com/reddit/client/redditclient2/controllers/listeners/TextSizeAndShareOnClickListener.java
@@ -1,11 +1,10 @@
 package com.reddit.client.redditclient2.controllers.listeners;
 
+import android.content.Intent;
 import android.view.View;
-import android.widget.Toast;
 
-import com.reddit.client.redditclient2.R;
+import com.reddit.client.redditclient2.api.things.Link;
 import com.reddit.client.redditclient2.controllers.activities.ArticlesActivity;
-import com.reddit.client.redditclient2.utils.TextSizes;
 
 /**
  * Created by raouf on 17-04-06.
@@ -13,15 +12,35 @@ import com.reddit.client.redditclient2.utils.TextSizes;
 
 public class TextSizeAndShareOnClickListener implements View.OnClickListener {
     private ArticlesActivity activity;
-
+    Link link = new Link();
+    int pos;
     public TextSizeAndShareOnClickListener(ArticlesActivity activity){
         this.activity = activity;
     }
 
     @Override
     public void onClick(View v) {
+
+
+        Intent Share = new Intent(android.content.Intent.ACTION_SEND);
+        Share.setType("text/plain");
+
+       /* pos= getIntent().getIntExtra("position",0);
+        String url1=lineup.children.get(pos).data.url;
+        url = lineup.children.get(pos).data.thumbnail;
+        stitre = lineup.children.get(pos).data.title;*/
+        String stitre = "la realité";
+        String url1=link.url;
+        // lineup=(RootData)Share.getSerializableExtra("object");
+        // Share.putExtra(android.content.Intent.EXTRA_SUBJECT,url);
+        Share.putExtra(android.content.Intent.EXTRA_TEXT, stitre);
+        //    try{
+        activity.startActivity(Intent.createChooser(Share, "shaire"));
+
+
+
         //Augmenter la taille du texte
-        if(v.getId() == R.id.text_size_up_button){
+     /*   if(v.getId() == R.id.text_size_up_button){
             TextSizes.FIRST_COMMENT_TITLES_SIZE++;
             TextSizes.FIRST_COMMENT_AUTHOR_SIZE += 0.5f;
             TextSizes.FIRST_COMMENT_SCORE_SIZE += 0.5f;
@@ -41,7 +60,7 @@ public class TextSizeAndShareOnClickListener implements View.OnClickListener {
             TextSizes.COMMENTS_SCORE_SIZE -= 0.5f;
 
             activity.getAdapter().notifyDataSetChanged();
-        }
+        }*/
     }
 
 }
